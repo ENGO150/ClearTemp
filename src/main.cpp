99a773ca6f223d53dc0c491756b67bb3da20b3d9
BuildTools <unconfigured@null.spigotmc.org>
2021-12-11 15:19:06 +0100

    remade main in c++

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
-#include <iostream>
-#include <fstream>
-#include <nlohmann/json.hpp>
+/*
+    This file runs void clearTemp() from src/tools/api.cpp with used args (except the first one [with index 0])
+*/
 
-using json = nlohmann::json;
+#include <list>
+#include <string>
+
+#include "../include/api.hpp"
 
 int main(int argc, char * argv[])
 {
-    json j;
-    std::fstream file("res/en.json", std::ios::in);
-    file >> j;
+    std::list<std::string> args;
 
-    std::cout << j["test"] << std::endl;
+    for (int i = 1; i < argc; i++)
+    {
+        args.push_back(argv[i]);
+    }
     
+    clearTemp(args);
+        
     return 0;
 }
